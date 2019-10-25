224a6963de35e289eceae6ea0691093e9af1618c
Léo Sarochar <56728067+LeoSarochar@users.noreply.github.com>
2019-10-25 13:13:06 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "You must run this with superuser priviliges.  Try \"sudo ./install.sh\"" 2>&1
+    exit 1
+else
+    echo "Installing Jankun..."
+fi
+
+sudo cp jankun /bin/jankun
+
+echo "Jankin installed (you might have to restart your terminal)"
